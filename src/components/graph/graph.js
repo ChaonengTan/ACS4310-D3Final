2f73d041cb6c60a6f7e0f2364b3a4c0b7cec118d
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-12-01 17:15:54 -0800

    numberParser, pieGen, pieGenData
    +number parser turns comma values into parseInt values
    
    +individual d3.pie() property

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -7,8 +7,8 @@ export default class Graph extends React.Component {
         this.myRef = React.createRef()
         this.state = {data:[]}
         this.testData = [
-            {category:'developer', reducer:'sales'},
-            {category:'publisher', reducer:'sales'}
+            {category:'publisher', reducer:'sales'},
+            {category:'title', reducer:'sales'}
         ]
     }
     
@@ -19,7 +19,6 @@ export default class Graph extends React.Component {
         const colorScale = d3.scaleSequential()
             .domain([0, data.length])
             .interpolator(d3.interpolateRainbow)
-        const arcData = (data, property) => d3.pie(data.map(d => d[property]))
         const arcGen = (innerRadius, outerRadius) => {
             return(
                 d3.arc()
@@ -29,8 +28,9 @@ export default class Graph extends React.Component {
             )
         }
         const countAllProperty = (data, category, reducer) => {
+            const parseNum = e => parseInt(e.split(',').join(''))
             const countedData = data.reduce((acc, obj) => {
-                acc[obj[category]] === undefined ? acc[obj[category]] = obj[reducer] : acc[obj[category]] += obj[reducer]
+                acc[obj[category]] === undefined ? acc[obj[category]] = parseNum(obj[reducer]) : acc[obj[category]] += parseNum(obj[reducer])
                 return acc
             }, [])
             let newParsedData = []
@@ -47,15 +47,16 @@ export default class Graph extends React.Component {
                 .attr('height', height)
                 .style('border', '1px black solid')
             this.testData.forEach((e, i) => {
+                // consts
                 const pieGroup = svg
                     .append('g')
-                    .attr('transform', `translate(${width * 2}, ${height / 2})`)
-                const piepath2 = pieGroup
+                    .attr('transform', `translate(${width / 2}, ${height / 2})`)
+                const pieGen = d3.pie()
+                const pieGenData = countAllProperty(data, e.category, e.reducer)
+                // drawer
+                const piepath = pieGroup
                     .selectAll('path')
-                    .data(() => {
-                        console.log(arcData(data, e.reducer))
-                        arcData(data, e.reducer)
-                    })
+                    .data(pieGen(pieGenData.map(d => d[e.reducer])))
                     .enter()
                     .append('path')
                     .attr('d', arcGen(40 + 200*i, 200 + 200*i))
@@ -63,15 +64,15 @@ export default class Graph extends React.Component {
             })
 
             // svg.selectAll('rect')
-            //         .data(data)
-            //         .enter()
-            //         .append('rect')
-            //         .attr('x', d => d.i * 30)
-            //         .attr('y', 600-40)
-            //         .attr('height', 40)
-            //         .attr('width', 20)
-            //         .attr('stroke', 'black')
-            //         .attr('fill', 'orange')
+            //     .data(data)
+            //     .enter()
+            //     .append('rect')
+            //     .attr('x', d => d.i * 30)
+            //     .attr('y', 600-40)
+            //     .attr('height', 40)
+            //     .attr('width', 20)
+            //     .attr('stroke', 'black')
+            //     .attr('fill', 'orange')
 
             // updater
             this.setState({data})
