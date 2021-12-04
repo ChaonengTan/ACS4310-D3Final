6cac7b7e0e493faeb90dde24fd960eb0297a457d
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-12-03 18:30:37 -0800

    width, height, graphSpacing, GraphSize interface
    you can now input width, height, graphSpacing, and graphSize
    
    svg and filtering options are initialized upon completing requisite initialization fields
    
    issue: cannot spam addFilter button and addFilter button seems to be lagging behind by 1 click

--- src/App.js
+++ src/App.js
@@ -10,29 +10,66 @@ const testData = [
 ]
 
 function App() {
+  // filterData
   const [filters, setFilters] = useState([]);
   const addNewFilter = data => setFilters([...filters, data])
-
   const [category, setCategory] = useState(null)
   const [reducer, setReducer] = useState(null)
-  return (
-    <div className="App">
-      <div>
-        <input type='text' onChange={e => setCategory(e.target.value)} placeholder='category'></input>
-        <input type='text' onChange={e => setReducer(e.target.value)} placeholder='reducer'></input>
-        <button onClick={e => {
-          addNewFilter({category:`${category}`, reducer:`${reducer}`})
-          // console.log(filters)
-        }}>Add Filter</button>
-      </div>
-      <Graph 
+  // initializationData
+  const [width, setWidth] = useState('600')
+  const [height, setHeight] = useState('600')
+  const [graphSpacing, setGraphSpacing] = useState('10')
+  const [graphSize, setGraphSize] = useState('100')
+  // graphData
+  const [graph, setGraph] = useState(null)
+  const setNewGraph = () => {
+    const graphStorage = () => {
+      return (
+        <Graph 
         data={gameInfo}
-        width='600'
-        height='600'
-        graphSpacing='10'
-        graphSize='100'
+        width={width}
+        height={height}
+        graphSpacing={graphSpacing}
+        graphSize={graphSize}
         filters={filters}
       />
+      )
+    }
+    setGraph(graphStorage())
+  }
+
+  // return
+  return (
+    <div className="App">
+      <div className='interface'>
+        {/* initialization */}
+        {!graph &&
+          <div className='initializer'>
+            <div className='graphSize'>
+              <input type='text' onChange={e => setWidth(e.target.value)} placeholder='width' value={width}></input>
+              <input type='text' onChange={e => setHeight(e.target.value)} placeholder='height' value={height}></input>
+            </div>
+            <div className='graphSettings'>
+              <input type='text' onChange={e => setGraphSpacing(e.target.value)} placeholder='graphSpacing' value={graphSpacing}></input>
+              <input type='text' onChange={e => setGraphSize(e.target.value)} placeholder='graphSize' value={graphSize}></input>
+            </div>
+            <button onClick={() => setNewGraph()}>Initialize Graph</button>
+          </div>
+        }
+        {/* generalInterface */}
+        {graph &&
+          <div className='addNewFilter'>
+            <input type='text' onChange={e => setCategory(e.target.value)} placeholder='category'></input>
+            <input type='text' onChange={e => setReducer(e.target.value)} placeholder='reducer'></input>
+            <button onClick={e => {
+              addNewFilter({category:`${category}`, reducer:`${reducer}`})
+              console.log(filters)
+              setNewGraph()
+            }}>Add Filter</button>
+        </div>
+        }
+      </div>
+      {graph}
     </div>
   );
 }
