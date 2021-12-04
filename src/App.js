2e6f3ff6232ef57569b72a715477b123c38bff68
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-12-03 17:19:55 -0800

    useState interface

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
+import { useState } from 'react';
 import Graph from './components/graph/graph'
 import gameInfo from './gameInfo.csv'
 
+const testData = [
+  {category:'publisher', reducer:'sales'},
+  {category:'title', reducer:'sales'},
+  {category:'platform', reducer:'sales'}
+]
+
 function App() {
-  const filters = [
-    {category:'publisher', reducer:'sales'},
-    {category:'title', reducer:'sales'},
-    {category:'platform', reducer:'sales'}
-  ]
+  const [filters, setFilters] = useState(testData);
+  const addNewFilter = data => setFilters([...filters, data])
+
+  const [category, setCategory] = useState(null)
+  const [reducer, setReducer] = useState(null)
   return (
     <div className="App">
+      <div>
+        <input type='text' onChange={e => setCategory(e.target.value)} placeholder='category'></input>
+        <input type='text' onChange={e => setReducer(e.target.value)} placeholder='reducer'></input>
+        <button onClick={e => {
+          addNewFilter({category:`${category}`, reducer:`${reducer}`})
+          // console.log(filters)
+        }}>Add Filter</button>
+      </div>
       <Graph 
         data={gameInfo}
         width='600'
