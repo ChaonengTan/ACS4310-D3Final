82a6f2d05c8bd1551fb4804b608847f79ee4124e
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-01-11 21:35:31 -0800

    +labels
    labels for inputs

--- src/App.js
+++ src/App.js
@@ -72,50 +72,73 @@ function App() {
   return (
     <div className="App">
       <div className='interface'>
+        <h1>Data Renderer</h1>
         {/* initialization */}
-        {/* {!graph && */}
-          <div className='initializer'>
+          <div className='interfaceGroup'>
             <select value={graphType} onChange={e => setGraphType(e.target.value)}>
               <option>BarGraph</option>
               <option>PieGraph</option>
             </select>
-            <div className='graphSize'>
-              <p>width</p>
-              <input type='number' onChange={e => setWidth(e.target.value)} placeholder='width' value={width}></input>
-              <p>height</p>
-              <input type='number' onChange={e => setHeight(e.target.value)} placeholder='height' value={height}></input>
+            {/* canvas dimentions */}
+            <div className='inputGroup'>
+              <label for='widthInput'>Width: 
+                <input id='widthInput' type='number' onChange={e => setWidth(e.target.value)} placeholder='width' value={width}></input>
+              </label>
+              <label for='heightInput'>Height: 
+                <input id='heightInput' type='number' onChange={e => setHeight(e.target.value)} placeholder='height' value={height}></input>
+              </label>
             </div>
+            {/* conditional rendering of graph-specific settings */}
             <div className='additionalSettings'>
               {graphType=='PieGraph' &&
-              <div className='pieGraphSettings'>
-                <p>graphSpacing</p>
-                <input type='number' onChange={e => setGraphSpacing(e.target.value)} placeholder='graphSpacing' value={graphSpacing}></input>
-                <p>graphSize</p>
-                <input type='number' onChange={e => setGraphSize(e.target.value)} placeholder='graphSize' value={graphSize}></input>
+              <div className='inputGroup'>
+                <label for='graphSpacingInput'>Graph Spacing:
+                  <input id='graphSpacingInput' type='number' onChange={e => setGraphSpacing(e.target.value)} placeholder='graphSpacing' value={graphSpacing}></input>
+                </label>
+                <label for='graphSizeInput'>Graph Size:
+                  <input id='graphSizeInput' type='number' onChange={e => setGraphSize(e.target.value)} placeholder='graphSize' value={graphSize}></input>
+                </label>
+                
               </div>
               }
               {graphType=='BarGraph' &&
-              <div className='barGraphSettings'>
-                <p>margin</p>
-                <input type='number' onChange={e => setMargin(e.target.value)} placeholder='margin' value={margin}></input>
+              <div className='inputGroup'>
+                <label for='marginInput'>Margin: 
+                  <input id='marginInput' type='number' onChange={e => setMargin(e.target.value)} placeholder='margin' value={margin}></input>
+                </label>
               </div>
               }
             </div>
-            <input type='file' id='customCSV' onChange={e => setCSV(URL.createObjectURL(e.target.files[0]))}></input>
-            <button onClick={() => setFilters([])}>Initialize Graph</button>
-            {graph &&
-            <button onClick={() => setGraph(null)}>clearGraph</button>
-            }
+            {/* graph initialization */}
+            <div className='inputGroup'>
+              <label for='csvInput'>Custom CSV: 
+                <input id='csvInput' type='file' id='customCSV' onChange={e => setCSV(URL.createObjectURL(e.target.files[0]))}></input>
+              </label>
+            </div>
+            <div className='inputGroup'>
+              <button onClick={() => setFilters([])}>Initialize Graph</button>
+              {graph &&
+              <button onClick={() => setGraph(null)}>clearGraph</button>
+              }
+            </div>
           </div>
-        {/* } */}
         {/* generalInterface */}
         {graph &&
-          <div className='addNewFilter'>
-            <input type='text' onChange={e => setCategory(e.target.value)} placeholder='category'></input>
-            <input type='text' onChange={e => setReducer(e.target.value)} placeholder='reducer'></input>
-            <button onClick={e => addNewFilter({category:`${category}`, reducer:`${reducer}`})}>Add Filter</button>
-            <div className='saveAs'>
-              <input type='text' placeholder='myFile.png' value={saveAsName} onChange={e => setSaveAsName(e.target.value)}></input>
+          <div className='interfaceGroup'>
+            <div className='inputGroup'>
+              <label for='categoryInput'>Category: 
+                <input id='categoryInput' type='text' onChange={e => setCategory(e.target.value)} placeholder='category'></input>
+              </label>
+              <label for='reducerInput'>Reducer: 
+                <input id='reducerInput' type='text' onChange={e => setReducer(e.target.value)} placeholder='reducer'></input>
+              </label>
+              
+              <button onClick={e => addNewFilter({category:`${category}`, reducer:`${reducer}`})}>Add Filter</button>
+            </div>
+            <div className='inputGroup'>
+              <label for='fileNameInput'>File Name: 
+                <input id='fileNameInput' type='text' placeholder='myFile.png' value={saveAsName} onChange={e => setSaveAsName(e.target.value)}></input>
+              </label>
               <button onClick={() => saveElement('graph', saveAsName)}>Save Image</button>
             </div>
           </div>
